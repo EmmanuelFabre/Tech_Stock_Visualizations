d1b9c02dd654bf103a4d9accc9ca79c85de67c3b
E-man <emmanuel.donohue@gmail.com>
2019-04-16 16:18:20 -0700

    cleaned up gitviz and deleted extra files

--- gitviz.js
+++ gitviz.js
@@ -9,35 +9,26 @@ var svg = d3
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom);
-  //.append("g")
-  //  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-//Should I create a 'chartGroup' ? To do so, comment out ln 11/12 from svg and put it in chartGroup
 
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
-//Read the data
-//d3.csv('trade.csv', function(data) {
+//Read the data from the csv
 d3.csv('trade.csv').then(data => {
 
   console.log(data);
-
-  console.log(data[0].Trading_Date)
+  //console.log(data[0].Trading_Date)
+  //Parse the dates for the X axis
   var parseTime = d3.timeParse("%m\/%d\/%y");
- // const tParser = d3.timeParse("%d/%m/%Y");
- // const date = tParser(data.Trading_Date);
 
   data.forEach(d => {
   //Parse the data
   //format the data to convert to numerical and date values
   //==========================================
   d.Trading_Date = parseTime(d.Trading_Date);
-  //  d.Trading_Date = +d.Trading_Date;                     //do we need to convert date to integer?
   d.Trading_Volume = +d.Trading_Volume;
-  //  or data.Trading_Date = d3.parseTime("%d-%b-%y");
-
   });
 
   console.log(data[0].Trading_Date)
@@ -61,32 +52,23 @@ d3.csv('trade.csv').then(data => {
      var myColor = d3.scaleOrdinal()
        .domain(allGroup)
        .range(d3.schemeSet2);
-    // var myColor = d3.scaleSequential()
-    //   .domain(allGroup)
-    //   .interpolator(d3.interpolateInferno);
       
 
     // Add X axis --> it is a date format
     var x = d3.scaleTime()
       .domain(d3.extent(data, d => d.Trading_Date))
-   //   .domain(["01/01/1998", d3.max(data, d => d.Trading_Date)])
-     // .domain(d3.extent(data, function(d) { return +d.Trading_Date; }))
-//      .domain(d3.extent(data, d => d.Trading_Date))
       .range([ 0, width]);
-//    svg.append("g")
+
     chartGroup.append("g")
       .attr("transform", "translate(0," + height + ")")
-    //  .call(d3.axisBottom(x).ticks(10));
-    //  .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%d-%b-%y")));
       .call(d3.axisBottom(x));
 
     
-
     // Add Y axis
     var y = d3.scaleLinear()
       .domain([0, d3.max(data, d => d.Trading_Volume)])
       .range([ height, 0 ]);
-//    svg.append("g")
+
     chartGroup.append("g")
       .call(d3.axisLeft(y));
 
@@ -99,21 +81,20 @@ d3.csv('trade.csv').then(data => {
           .x(function(d) { return x(d.Trading_Date) + margin.left })
           .y(function(d) { return y(d.Trading_Volume) })
         )
-        .attr("stroke", function(d){ return myColor("value") })       //PB seems to do nothing?
-    //    .attr("stroke", function(d){ return myColor(d.Ticker) })
+        .attr("stroke", function(d){ return myColor("value") })      
         .style("stroke-width", 2)
-    //    .style("fill", "none")
+        .style("fill", "none")
         .style("stroke", "blue");
 
-    // A function that updates the chart
+    // A function to update the chart based on filter button choice
     function update(selectedGroup) {
 
       console.log("update function works");
 
-      // Create new data with the selection?
+      // Create new data with the selection
       var dataFilter = data.filter(function(d){return d.Ticker==selectedGroup});
 
-      // Give these new data to update line
+      // Update line with the new data
       line
           .datum(dataFilter)
           .transition()
@@ -134,9 +115,5 @@ d3.csv('trade.csv').then(data => {
     })
 
 })
-
-
-
-//PB with myColor, "ValueA"
 
 
